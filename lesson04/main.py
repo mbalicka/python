947f84c5b53aac663789e56ad9d5c2d97f84f04f
Maria Balicka <mariasz123@gmail.com>
2018-04-15 21:24:19 +0200

    Add lesson04
    Signed-off-by: Maria Balicka <mariasz123@gmail.com>

---
+++ lesson04/main.py
@@ -0,0 +1,10 @@
+def main():
+    table = []
+    table.append("skin")
+    table.append("to")
+    table.append("skin")
+    for x in table:
+        print(x)
+
+if __name__ == "__main__":
+     main()
